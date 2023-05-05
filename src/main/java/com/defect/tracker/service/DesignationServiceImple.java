b3a94b65fa4cf4b3c3ff91d60437a5bf28fa0b3b
Akalya97 <akalya.gnanasekaram@gmail.com>
2023-05-05 11:41:55 +0530

    changes

--- src/main/java/com/defect/tracker/service/DesignationServiceImple.java
+++ src/main/java/com/defect/tracker/service/DesignationServiceImple.java
@@ -2,15 +2,20 @@ package com.defect.tracker.service;
 
 import com.defect.tracker.dto.DesignationRequest;
 import com.defect.tracker.dto.DesignationResponse;
+import com.defect.tracker.repository.DesignationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DesignationServiceImple implements DesignationService{
+
+    @Autowired
+    private DesignationRepository designationRepository;
     @Override
     public void createDesignation(DesignationRequest designationRequest) {
-        
+
     }
 
     @Override
